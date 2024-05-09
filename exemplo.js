8b5e32d7ca07eda85d39c8ad106d26378cf2521c
DANIEL DE FREITAS PASSETI PATRICIO <39969184881@info.local>
2024-05-09 08:54:24 -0300

    feaat: exercicio 1

---
+++ exemplo.js
@@ -0,0 +1,17 @@
+//console.log("Hello world!!");
+
+//prompt("Digite alguma coisa: ")
+
+
+//let nome = prompt("Olá, digite seu nome: ")
+
+//console.log("Seja bem vindo" + nome + "!!!")
+
+//let pontos = 100;
+
+//let bonus = parseInt(prompt("Quantos pontos você deseja somar?"))
+
+//let totalPontos = pontos + bonus;
+//console.log(totalPontos)
+//let notaAluno = parseFloat(prompt("DIgite nota do aluno"));
+//console.log(notaAluno);
